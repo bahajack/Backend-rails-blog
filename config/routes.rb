bea3b9b06f0a1ca680a05ea2cb6fc2ec6d429f9a
Ubuntu <ubuntu@ip-172-31-7-60.ap-northeast-1.compute.internal>
2021-04-29 07:04:29 +0000

    final commit for lesson6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show' 
+  resources :pages
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
